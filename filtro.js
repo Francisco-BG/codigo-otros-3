09c74d8a73ec8d41d3d73f76b56543c0ebf947ec
Francisco BG <frank.betgar@gmail.com>
2023-02-13 10:58:59 -0600

    ejercicio a medias, buen intento

--- filtro.js
+++ filtro.js
@@ -1,59 +1,62 @@
 // Tenemos un li de productos
 
 const productos = [
-  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
-  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
-  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
-  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
-  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+  { nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg" },
+  { nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg" },
+  { nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg" },
+  { nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg" },
+  { nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg" }
 ]
 
-const li = document.getElementsByName("lista-de-productos")
+const li = document.getElementById("lista-de-productos") //lista-de-productos es un id, la funcion hacíá referencia a un nombre
 const $i = document.querySelector('.input');
+/*Faltaba la definición de displayProductos */
+function displayProductos(productos){
+  for (let i = 0; i < productos.length; i++) {
+    //let li =document.createElement("li") 
+    var d = document.createElement("div")
+    d.classList.add("producto")
 
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+    var ti = document.createElement("p")
+    ti.classList.add("titulo")
+    ti.textContent = productos[i].nombre
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
-  
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productos[i].img);
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+    d.appendChild(ti)
+    d.appendChild(imagen)
 
-  li.appendChild(d)
+    li.appendChild(d) //No se ha creado el elemento lista, para hacer la lista de productos
+  }
 }
 
-displayProductos(productos)
+displayProductos(productos);//Se llamaba la función sin estar definida
 const botonDeFiltro = document.querySelector("button");
 
-botonDeFiltro.onclick = function() {
+botonDeFiltro.onclick = function () {
   while (li.firstChild) {
     li.removeChild(li.firstChild);
   }
 
   const texto = $i.value;
   console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
+  const productosFiltrados = filtrado(productos, texto);
 
   for (let i = 0; i < productosFiltrados.length; i++) {
     var d = document.createElement("div")
     d.classList.add("producto")
-  
+
     var ti = document.createElement("p")
     ti.classList.add("titulo")
     ti.textContent = productosFiltrados[i].nombre
-    
+
     var imagen = document.createElement("img");
     imagen.setAttribute('src', productosFiltrados[i].img);
-  
+
     d.appendChild(ti)
     d.appendChild(imagen)
-  
+
     li.appendChild(d)
   }
 }
